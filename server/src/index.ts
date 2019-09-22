ce1a9ee8f6d951d119bc0416dfbb5ae41c683537
siddharthasarma <sidduinevitable@gmail.com>
2019-09-22 21:35:48 +0530

    add graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,23 @@
 import "reflect-metadata";
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 
 (async () => {
   const app = express();
   app.get('/', (_, res) => res.send('hello'));
+  const apolloServer = new ApolloServer({
+    typeDefs: `
+      type Query {
+        hello: String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'hello world'
+      }
+    }
+  });
+  apolloServer.applyMiddleware({ app });
   app.listen('4000', () => {
     console.log('Server running at port 4000');
   });
