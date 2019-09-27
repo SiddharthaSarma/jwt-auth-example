7539bb2936334e6a3d76f75829d9210aaf342318
siddharthasarma <sidduinevitable@gmail.com>
2019-09-27 07:08:17 +0530

    add functionality to send the jwt token

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,7 +1,13 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
-import { hash } from 'bcryptjs';
+import { Resolver, Query, Mutation, Arg, ObjectType, Field } from 'type-graphql';
+import { hash, compare } from 'bcryptjs';
 import { User } from './entity/User';
+import { sign } from 'jsonwebtoken';
 
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
 @Resolver()
 export class UserResolver {
   @Query(() => String)
@@ -12,6 +18,26 @@ export class UserResolver {
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email', () => String) email: string,
+    @Arg('password', () => String) password: string,
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: email });
+
+    if (!user) {
+      throw new Error('User is not valid');
+    }
+    const valid = await compare(user.password, password);
+
+    if (!valid) {
+      throw new Error('bad password')
+    }
+    return {
+      accessToken: sign({ userId: user.id }, 'asdfghghgls', { expiresIn: '15m' })
+    };
   }
 
   @Mutation(() => Boolean)
